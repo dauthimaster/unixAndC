65ac0459e1f92a5d5dc33e15913cf56da60f6b87
dauthimaster <dauthimaster@gmail.com>
2013-02-07 01:12:05 -0600

    fixed infinite loop error

--- count/count.c
+++ count/count.c
@@ -2,16 +2,17 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
+    //the count for all the digits starts at 0.
     int count[10] = {[0 ... 9] = 0};
     char c;
     
-    do{
+    do{ //get each number char, see if it's a digit and count it if it is
         c=getchar();
         int i = c - '0';
         if(i >= 0 && i < 10){
             ++count[i];
         }
-    }while(c != EOF);
+    }while(c != '\n');
 
     printf("0 1 2 3 4 5 6 7 8 9\n");
     
