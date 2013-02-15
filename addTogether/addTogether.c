d4880df7304e4cc6ace15fd30322d4de9086cba4
dauthimaster <dauthimaster@gmail.com>
2013-02-15 01:54:28 -0600

    adding hw done

---
+++ addTogether/addTogether.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char * argv[]){
+    if(argc > 1){
+        int i, tot;
+
+        tot = 0;
+
+        for(i=1; i<argc; ++i){
+            tot += strtol(argv[i], NULL, 10);
+        }
+
+        printf("sum is %d\n", tot);
+    }
+
+    return 0;
+}
